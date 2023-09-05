facdacf109b2f2f44731d7d1d45c21e8aa749b64
muknan <muk.msi@gmail.com>
2023-09-05 12:48:59 +0530

    Resetting properties in JS

--- script.js
+++ script.js
@@ -16,12 +16,26 @@ const numberElem = document.querySelector('.number');
 const highscoreElem = document.querySelector('.highscore');
 const resetBtn = document.querySelector('.reset');
 
+const initMessage = messageElem.textContent;
+const initNumber = numberElem.textContent;
+
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+
 // console.log(secretNumber);
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.location.reload();
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  scoreElem.textContent = score;
+  messageElem.textContent = initMessage;
+  numberElem.textContent = initNumber;
+  document.body.style.backgroundColor = '';
+  numberElem.style.width = '';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.guess').focus();
+
+  // document.location.reload();
 });
 
 document.querySelector('.guess').addEventListener('keyup', function () {
@@ -58,8 +72,8 @@ checkBtn.addEventListener('click', function () {
   } else {
     messageElem.textContent = '🎉 You guessed it right!';
     numberElem.textContent = secretNumber;
-    document.body.style = 'background-color: #66B367';
-    numberElem.style = 'width: 30rem';
+    document.body.style.backgroundColor = '#51A65B';
+    numberElem.style.width = '30rem';
 
     let newScore = score;
     if (newScore >= Number(highscoreElem.textContent)) {
